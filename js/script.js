1ad258e48d4e9d48b98f649fbd5d9af5f788f450
off-art <troy750@yandex.ru>
2019-11-28 22:16:56 +0300

    add animations

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $('.schedule__link').click(function (e) {
   e.preventDefault();
   $('.gifts').toggle();
 });
+
+
 $('.gift__add').click(function (e) {
   e.preventDefault();
   $('.gift-hidden').toggle();
@@ -14,7 +16,7 @@ $(document).ready(function () {
     loop: true,
     margin: 10,
     nav: false,
-    autoplay:true,
+    autoplay: true,
     responsive: {
       0: {
         items: 1
@@ -22,3 +24,28 @@ $(document).ready(function () {
     }
   });
 });
+
+var $buoop = {
+  required: {
+    e: -4,
+    f: -3,
+    o: -3,
+    s: -1,
+    c: -3
+  },
+  insecure: true,
+  unsupported: true,
+  api: 2019.11
+};
+
+function $buo_f() {
+  var e = document.createElement("script");
+  e.src = "//browser-update.org/update.min.js";
+  document.body.appendChild(e);
+}
+
+try {
+  document.addEventListener("DOMContentLoaded", $buo_f, false);
+} catch (e) {
+  window.attachEvent("onload", $buo_f);
+}
